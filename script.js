a52ffc64f5869ab5c9c9db4f856e821193b278b7
Sonu Rao <srao@Sonus-MacBook-Air.local>
2026-04-04 00:19:26 -0500

    Update files

--- script.js
+++ script.js
@@ -133,3 +133,43 @@ document.addEventListener('DOMContentLoaded', function() {
     lastScroll = currentScroll;
   });
 });
+<<<<<<< HEAD
+=======
+const images = document.querySelectorAll(".gallery-img");
+const lightbox = document.getElementById("lightbox");
+const lightboxImg = document.getElementById("lightbox-img");
+
+let currentIndex = 0;
+
+// OPEN IMAGE
+images.forEach((img, index) => {
+  img.addEventListener("click", () => {
+    lightbox.style.display = "block";  // instead of "block"    lightboxImg.src = img.src;
+    currentIndex = index;
+  });
+});
+
+// CLOSE
+document.querySelector(".close").onclick = () => {
+  lightbox.style.display = "none";
+};
+
+// NEXT
+document.querySelector(".next").onclick = () => {
+  currentIndex = (currentIndex + 1) % images.length;
+  lightboxImg.src = images[currentIndex].src;
+};
+
+// PREV
+document.querySelector(".prev").onclick = () => {
+  currentIndex = (currentIndex - 1 + images.length) % images.length;
+  lightboxImg.src = images[currentIndex].src;
+};
+
+// CLOSE ON BACKGROUND CLICK
+lightbox.onclick = (e) => {
+  if (e.target === lightbox) {
+    lightbox.style.display = "none";
+  }
+};
+>>>>>>> 685c4d8 (Initial commit with updates)
